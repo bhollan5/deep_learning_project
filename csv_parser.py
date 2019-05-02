bcd9a4f4e1531c10bf2167aad9d62ddcd7e0199c
Ben Holland <bhollan5@kent.edu>
2019-05-01 21:58:30 -0400

    Separated test and training data into dataframes

--- csv_parser.py
+++ csv_parser.py
@@ -1,12 +1,24 @@
-import csv
+import numpy as np
+import pandas as pd
 
-with open('events.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+#   Grabbing the entire data set
+data_set =  pd.read_csv('events.csv')
+print(data_set.head())
+
+#   Getting the latest timestamp, which we'll use to 
+#   calculate final 24 hour's worth of data
+latest_time = data_set['timestamp'].max()
+
+#   Constant defining milliseconds in a day
+MILLISECOND_DAY = 86400000
+
+#   Figuring out the cutoff between training and test  data:
+time_cutoff = latest_time - MILLISECOND_DAY
+
+#   Separating the data
+training_data = data_set[data_set['timestamp'] < time_cutoff]
+test_data = data_set[data_set['timestamp'] > time_cutoff]
+print("Training:")
+print(training_data.shape)
+print("Test:")
+print(test_data.shape)
